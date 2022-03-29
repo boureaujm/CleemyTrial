fa3222b2fa2d8420069464c2e72ee472d1fc59de
Jean Michel Boureau <jm.boureau@e.email>
2022-03-30 00:23:06 +0200

    add error messages

--- CleemyCommons/Types/PaymentConstants.cs
+++ CleemyCommons/Types/PaymentConstants.cs
@@ -4,5 +4,10 @@
     {
         public const string CST_AMOUNT = "Amount";
         public const string CST_DATE = "Date";
+
+        public const string CST_ERROR_INCOHERENT_CURRENCY = "Payment currency and user currency must be the same";
+        public const string CST_ERROR_DUPLICATE_PAYMENT = "Duplicate payment";
+        public const string CST_ERROR_CURRENCY_NOT_EXIST = "Currency does not exist";
+        public const string CST_ERROR_USER_NOT_EXIST = "User does not exist";
     }
 }
